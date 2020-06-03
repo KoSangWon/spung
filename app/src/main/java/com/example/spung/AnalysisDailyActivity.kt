4e1d328459586fa19ae0c3fc9c7ccabffbef7693
DongGon <58687439+KimDongGon@users.noreply.github.com>
2020-06-03 20:57:43 +0900

    activity간 이동
    activity간 이동

--- app/src/main/java/com/example/spung/AnalysisDailyActivity.kt
+++ app/src/main/java/com/example/spung/AnalysisDailyActivity.kt
@@ -1,18 +1,23 @@
 package com.example.spung
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import kotlinx.android.synthetic.main.activity_analysis_daily.*
 
 class AnalysisDailyActivity : AppCompatActivity() {
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.menu, menu)
-        return true
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_analysis_daily)
+        init()
+    }
+
+    private fun init() {
+        morebtn.setOnClickListener {
+            var i = Intent(this, AnalysisMonthlyActivity::class.java)
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/spung/SelectGameActivity.kt
+++ app/src/main/java/com/example/spung/SelectGameActivity.kt
@@ -1,12 +1,30 @@
 package com.example.spung
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_select_game.*
 
 class SelectGameActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_game)
+        init()
+    }
+
+    private fun init() {
+        rhythmgamebtn.setOnClickListener {
+            var i = Intent(this, RhythmGameActivity::class.java)
+            startActivity(i)
+        }
+        pillowgamebtn.setOnClickListener {
+            var i = Intent(this, PillowGameActivity::class.java)
+            startActivity(i)
+        }
+        molegamebtn.setOnClickListener {
+            var i = Intent(this, MoleGameActivity::class.java)
+            startActivity(i)
+        }
     }
 }
