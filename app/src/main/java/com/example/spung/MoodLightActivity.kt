80e1a179a532467875a12dead0d8994620fb38fd
DongGon <58687439+KimDongGon@users.noreply.github.com>
2020-06-20 23:33:42 +0900

    add 기능

--- app/src/main/java/com/example/spung/MoodLightActivity.kt
+++ app/src/main/java/com/example/spung/MoodLightActivity.kt
@@ -1,18 +1,118 @@
 package com.example.spung
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.ActionBar
+import app.akexorcist.bluetotohspp.library.BluetoothSPP
+import app.akexorcist.bluetotohspp.library.BluetoothState
+import app.akexorcist.bluetotohspp.library.DeviceList
+import kotlinx.android.synthetic.main.activity_bluetooth.*
+import kotlinx.android.synthetic.main.activity_bluetooth_speaker.*
+import kotlinx.android.synthetic.main.activity_mood_light.*
 import kotlinx.android.synthetic.main.custom_action_bar_layout.*
 
 class MoodLightActivity : AppCompatActivity() {
+    lateinit var bt: BluetoothSPP
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mood_light)
         setActionBar()
+        init()
+    }
+
+    private fun init() {
+        bt = BluetoothSPP(this)
+        if(!bt.isBluetoothAvailable){
+            Toast.makeText(this, "can't use bluetooth", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+
+        bt.setOnDataReceivedListener { Data, message ->
+            Toast.makeText(applicationContext, message, Toast.LENGTH_SHORT).show()
+        }
+
+        bt.setBluetoothConnectionListener(object:BluetoothSPP.BluetoothConnectionListener{
+            override fun onDeviceDisconnected() {
+                Toast.makeText(applicationContext, "Bluetooth is disconnected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDeviceConnected(name: String?, address: String?) {
+                Toast.makeText(applicationContext, "Bluetooth is connected", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onDeviceConnectionFailed() {
+                Toast.makeText(applicationContext, "Bluetooth connection is failed", Toast.LENGTH_SHORT).show()
+            }
+        })
+        moodlightOnOffbtn.setOnClickListener {
+            if(bt.serviceState == BluetoothState.STATE_CONNECTED){
+                bt.disconnect()
+            }
+            else{
+                var i = Intent(applicationContext, DeviceList::class.java)
+                startActivityForResult(i, BluetoothState.REQUEST_CONNECT_DEVICE)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        bt.stopService()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(!bt.isBluetoothEnabled){
+            bt.enable()
+        }
+        else{
+            if(!bt.isServiceAvailable){
+                bt.setupService()
+                bt.startService(BluetoothState.DEVICE_OTHER)
+                setup()
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == BluetoothState.REQUEST_CONNECT_DEVICE){
+            if(resultCode == Activity.RESULT_OK)
+                bt.connect(data)
+        }
+        else if(requestCode == BluetoothState.REQUEST_ENABLE_BT){
+            if(resultCode == Activity.RESULT_OK){
+                Toast.makeText(this,"yes",Toast.LENGTH_SHORT).show()
+                bt.setupService()
+            }
+            else{
+                Toast.makeText(this,"no",Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    fun setup(){
+        var blue = false
+//        redbtn.setOnClickListener {
+//
+//            bt.send("1",false)
+//        }
+        bluebtn.setOnClickListener {
+            if(blue){
+                bt.send("0",false)
+                blue = false
+            }
+            else {
+                bt.send("1", false)
+                blue = true
+            }
+        }
     }
 
     private fun setActionBar() {
